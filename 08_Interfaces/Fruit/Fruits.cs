9d74c9a70df78f7b7c570c99bfdbc6fe2a804336
Jesse Smith <worldjesses@gmail.com>
2021-05-06 15:21:09 -0400

    Interface Added

---
+++ 08_Interfaces/Fruit/Fruits.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Interfaces.Fruit
+{
+
+    public class Banana : IFruit
+    {
+
+        // Constructors
+        public Banana() { }
+        public Banana(bool isPeeled)
+        {
+            IsPeeled = isPeeled;
+        }
+
+        // Properties
+        public string Name
+        {
+            get
+            {
+                return "Banana";
+            }
+        }
+
+        public bool IsPeeled { get; private set; }
+
+        // Class Method
+        public string Peel()
+        {
+            IsPeeled = true;
+            return "You peeled the banana";
+        }
+
+    }
+
+    public class Orange : IFruit
+    {
+        // Constuctors
+
+        public Orange() { }
+        public Orange(bool isPeeled)
+        {
+            IsPeeled = isPeeled;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Orange";
+            }
+        }
+
+        public bool IsPeeled { get; private set; }
+
+        public string Peel()
+        {
+            if (IsPeeled)
+            {
+                return "It's already peeled";
+            }
+            else
+            {
+                IsPeeled = true;
+                return "You peeled the orange";
+            }
+        }
+
+        // Classes that use interfaces can still have unique properties and methods.
+        public string Squeeze()
+        {
+            return "You squeeze the orange, and juice comes our";
+        }
+    }
+
+    public class Grape : IFruit
+    {
+        public string Name
+        {
+            get
+            {
+                return "Grape";
+            }
+        }
+        // Hardsetting preoperty as false
+        public bool IsPeeled { get; } = false;
+
+        public string Peel()
+        {
+            return "Who peels grapes?";
+        }
+    }
+
+    // Make an apple class inheriting from IFruit challenge
+
+    public class Apple : IFruit
+    {
+        public Apple() { }
+        public bool IsPeeled { get; set; }
+        public Apple(bool isPeeled)
+        {
+            IsPeeled = isPeeled;
+        }
+        public string Peel()
+        {
+            IsPeeled = true;
+            return "You peeled the Apple";
+        }
+        // => Lambda or 'phat arrow'
+        // public string Name => "Apple";
+        public string Name
+        {
+            get
+            {
+                return "Apple";
+            }
+        }
+        public string Squeeze()
+        {
+            return "And you made apple juice?";
+        }
+        public string Compare(Orange orange)
+        {
+            return "You can't compare these";
+        }
+    }
+}
+
+
+
+
+
+
---
+++ 08_Interfaces/IFruitTests.cs
@@ -0,0 +1,123 @@
+﻿using _08_Interfaces.Fruit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace _08_Interfaces
+{
+    [TestClass]
+    public class IFruitTests
+    {
+        [TestMethod]
+        public void CallingInterfaceMethods()
+        {
+            IFruit banana = new Banana();
+            Banana banana1 = new Banana();
+
+            // Can't create an interface object
+            // IFruit banana = new IFruit();
+
+            // var is like a wildcard arguement
+            var output = banana.Peel();
+            Console.WriteLine(output);
+
+            Console.WriteLine("The banana is peeled: " + banana.IsPeeled);
+            Assert.IsTrue(banana.IsPeeled);
+        }
+
+        [TestMethod]
+        public void InterfacesInCollections()
+        {
+            Orange orange = new Orange();
+
+            //Var allows different fruits using the IFruit interface to exist together
+            var fruitSalad = new List<IFruit>
+            {
+                new Banana(),
+                new Orange(),
+                orange
+            };
+
+            // Orange exclusive methods still accessible outside of IFruit collection
+            orange.Squeeze();
+
+            foreach (var fruit in fruitSalad)
+            {
+                Console.WriteLine(fruit.Name);
+                Console.WriteLine(fruit.Peel());
+                // no longer accessible once in a collection
+                // fruit.squeeze
+
+                Assert.IsInstanceOfType(fruit, typeof(IFruit));
+            }
+
+            Assert.IsInstanceOfType(orange, typeof(Orange));
+        }
+
+        private string GetFruitName(IFruit fruit)
+        {
+            return $"This fruit is called {fruit.Name}";
+        }
+
+        [TestMethod]
+        public void InterfacesInMethods()
+        {
+            var grape = new Grape();
+
+            // Even though the method only takes IFruit, grape still qualifies
+            string output = GetFruitName(grape);
+
+            Console.WriteLine(output);
+        }
+
+        [TestMethod]
+        public void TypeOfInstance()
+        {
+            var fruitSalad = new List<IFruit>
+            {
+                new Orange(true),
+                new Orange(false),
+                new Grape(),
+                new Orange(),
+                new Banana(true),
+                new Grape()
+            };
+
+            Console.WriteLine("Is the orange peeled?");
+            foreach (var fruit in fruitSalad)
+            {   // Checking it's of type orange, casting it as orange
+                // Pattern Matching
+                if (fruit is Orange orange)
+                {
+                    if (orange.IsPeeled)
+                    {
+                        Console.WriteLine("It's a peeled orange");
+                        // Regain orange exclusive properties
+                        // ((Orange)fruit).Squeeze();
+                        orange.Squeeze();
+                    }
+                    else
+                    {
+                        Console.WriteLine("It's an orange");
+                    }
+                }
+                else if (fruit.GetType() == typeof(Grape))
+                {
+                    Console.WriteLine("It's a grape");
+
+                    var grape = (Grape)fruit;
+                    Console.WriteLine(grape.Peel());
+                }
+                else if (fruit.IsPeeled)
+                {
+                    Console.WriteLine("It's a peeled banana");
+                }
+                else
+                {
+                    Console.WriteLine("It's a Banana");
+                }
+            }
+        }
+    }
+}
+
